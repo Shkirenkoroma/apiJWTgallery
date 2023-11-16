28ddd0a556740b313ff8a614ebe41528991ca1b8
Shkirenkoroma <shkirenkoroma@mail.ru>
2023-11-16 15:08:53 +0300

    fix/JWT: added settings for eslint and prettier configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
+import { FC } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
-}
+const App: FC = (): JSX.Element => {
+  return <div className="App">Привет</div>;
+};
 
 export default App;
