09d10ffb9da377aa21261901a2ecdb205a99730c
Shkirenkoroma <shkirenkoroma@mail.ru>
2023-11-16 15:41:26 +0300

    fix/JWT: added settings for eslint

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,15 @@ import { FC } from 'react';
 import './App.css';
 
 const App: FC = (): JSX.Element => {
-  return <div className="App">Привет</div>;
+  return (
+    <div className="App">
+      <div>
+        <div>
+          <div>!</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
