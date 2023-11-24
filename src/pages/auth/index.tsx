ee43056bc11a55c14deae9388bdea3dfba41b23d
Shkirenkoroma <shkirenkoroma@mail.ru>
2023-11-24 21:59:24 +0300

    feature/JWT: added input elements

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { MDBInput } from 'mdb-react-ui-kit';
 
@@ -13,14 +13,130 @@ const initialState = {
 const Auth: FC = (): JSX.Element => {
   const [formValue, setFormValue] = useState(initialState);
   const [showRegister, setShowRegister] = useState(false);
+  const { firstName, lastName, email, password, confirmPassword } = formValue;
 
-  useEffect(() => {}, []);
+  const handleChange = (e: any): void => {
+    setFormValue({ ...formValue, [e.target.name]: e.target.value });
+  };
 
   return (
     <section className="vh-100 gradient-custom">
       <div className="container py-4 h-100">
         <div className="row d-flex justify-content-center align-items-center h-100">
-          
+          <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div
+              className="card bg-dark text-white"
+              style={{ borderRadius: '1rem' }}
+            >
+              <div className="card-body p-4 text-center">
+                <div className="mb-md-5 mt-md-4 pb-5">
+                  <h2 className="fw-bold mb-2 text-uppercase">
+                    {!showRegister ? 'Login' : 'Register'}
+                  </h2>
+                  <p className="text-white mb-4">
+                    {!showRegister
+                      ? 'Please enter your Email & Password'
+                      : 'Please enter User detail'}
+                  </p>
+                  {showRegister && (
+                    <>
+                      <div className="form-outline form-white mb-4">
+                        <MDBInput
+                          type="text"
+                          name="firstName"
+                          value={firstName}
+                          onChange={handleChange}
+                          label="First Name"
+                          className="form-control form-control-lg"
+                        />
+                      </div>
+                      <div className="form-outline form-white mb-4">
+                        <MDBInput
+                          type="text"
+                          name="lastName"
+                          value={lastName}
+                          onChange={handleChange}
+                          label="Last Name"
+                          className="form-control form-control-lg"
+                        />
+                      </div>
+                    </>
+                  )}
+                  <div className="form-outline form-white mb-4">
+                    <MDBInput
+                      type="email"
+                      name="email"
+                      value={email}
+                      onChange={handleChange}
+                      label="Email"
+                      className="form-control form-control-lg"
+                    />
+                  </div>
+                  <div className="form-outline form-white mb-4">
+                    <MDBInput
+                      type="password"
+                      name="password"
+                      value={password}
+                      onChange={handleChange}
+                      label="Password"
+                      className="form-control form-control-lg"
+                    />
+                  </div>
+                  {showRegister && (
+                    <div className="form-outline form-white mb-4">
+                      <MDBInput
+                        type="password"
+                        name="confirmPassword"
+                        value={confirmPassword}
+                        onChange={handleChange}
+                        label="Confirm Password"
+                        className="form-control form-control-lg"
+                      />
+                    </div>
+                  )}
+                  {!showRegister ? (
+                    <button
+                      className="btn btn-outline-light btn-lg px-5"
+                      type="button"
+                    >
+                      Login
+                    </button>
+                  ) : (
+                    <button className="btn btn-outline-light btn-lg px-5">
+                      Register
+                    </button>
+                  )}
+                </div>
+                <div>
+                  <h5 className="mb-0">
+                    {!showRegister ? (
+                      <>
+                        Don`t have an account ?
+                        <p
+                          className="text-white-50 fw-bold"
+                          style={{ cursor: 'pointer' }}
+                          onClick={() => setShowRegister(true)}
+                        >
+                          Sign Up
+                        </p>
+                      </>
+                    ) : (
+                      <>
+                        Already have an account ?
+                        <p
+                          className="text-white-50 fw-bold"
+                          style={{ cursor: 'pointer' }}
+                          onClick={() => setShowRegister(false)}
+                        >
+                          Sign In
+                        </p>
+                      </>
+                    )}
+                  </h5>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
